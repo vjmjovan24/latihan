189c3c7241a6a3fa858d4e83b3fd1b14d00b2c87
Vinsensius Jovan <marta.jovan24@gmail.com>
2026-07-21 11:18:33 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print(){
+	printf("Hallo Nama Saya Vinsensius Jovan\n");
+	printf("Bahasa Pemrograman yang Saya Suka C dan Javascript");
+}
+
+int main(){
+	print();
+	
+	return 0;
+}
